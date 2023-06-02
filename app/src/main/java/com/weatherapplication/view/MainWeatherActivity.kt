4faa6b20721214cb52581a17e7884dd2926e08be
IhorB <igorbat15@hotmail.com>
2023-06-02 14:42:35 -0400

    Initial project setup

---
+++ app/src/main/java/com/weatherapplication/view/MainWeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.weatherapplication.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainWeatherActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
